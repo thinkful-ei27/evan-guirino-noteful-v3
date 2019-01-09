18ee6cf2bc24e7f57dec560e911d58393f24713d
Evan Guirino <etg624@gmail.com>
2019-01-09 14:47:00 -0800

    deploying

--- test/app.test.js
+++ test/app.test.js
@@ -13,7 +13,6 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Reality Check', () => {
-
   it('true should be true', () => {
     expect(true).to.be.true;
   });
@@ -21,42 +20,36 @@ describe('Reality Check', () => {
   it('2 + 2 should equal 4', () => {
     expect(2 + 2).to.equal(4);
   });
-
 });
 
 describe('Environment', () => {
-
   it('NODE_ENV should be "test"', () => {
     expect(process.env.NODE_ENV).to.equal('test');
   });
-
 });
 
 describe('Basic Express setup', () => {
-
   describe('Express static', () => {
-
     it('GET request "/" should return the index page', () => {
-      return chai.request(app)
+      return chai
+        .request(app)
         .get('/')
-        .then(function (res) {
+        .then(function(res) {
           expect(res).to.exist;
           expect(res).to.have.status(200);
           expect(res).to.be.html;
         });
     });
-
   });
 
   describe('404 handler', () => {
-
     it('should respond with 404 when given a bad path', () => {
-      return chai.request(app)
+      return chai
+        .request(app)
         .get('/bad/path')
         .then(res => {
           expect(res).to.have.status(404);
         });
     });
-
   });
 });
